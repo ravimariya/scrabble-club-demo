e0c719c692feac660a72dd678532eb0293e81ce2
ravimariya <rm9492@outlook.com>
2021-09-11 17:55:47 +0530

    changes in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ email = %w[jones.walton@ex.com eva.taylor@ex.com, valencia.murry@ex.com]
 phone_numbers = %w[1234567890 9867854422 1234567898]
 date = Date.today
 
-3.times {|m| Member.create(first_name: first_name[m], last_name: last_name[m], email: email[m], phone_numbers: phone_number[m])}
+3.times {|m| Member.create(first_name: first_name[m], last_name: last_name[m], email: email[m], phone_number: phone_numbers[m])}
 member_ids = Member.ids
 
 5.times {|g| Game.create!(title: "Game #{g+1}", end_at: date, participants_attributes: [{member_id: member_ids[0], score: rand(100)}, {member_id: member_ids[1], score: rand(100)}]) }
